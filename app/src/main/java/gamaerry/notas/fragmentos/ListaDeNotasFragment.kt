2181919079a76fb66142183b6037429d89c64f3c
gamaerry <gamaerry@gmail.com>
2023-05-27 15:36:00 -0600

    completado el fragment ListaDeNotas

--- app/src/main/java/gamaerry/notas/fragmentos/ListaDeNotasFragment.kt
+++ app/src/main/java/gamaerry/notas/fragmentos/ListaDeNotasFragment.kt
@@ -1,32 +1,87 @@
-package gamaerry.notas.fragmentos
+package gamaerry.notas.adaptadores
 
-import android.os.Bundle
+import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import gamaerry.notas.databinding.FragmentListaDeNotasBinding
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import gamaerry.notas.databinding.ItemNotaBinding
+import gamaerry.notas.datos.Nota
 
-class ListaDeNotasFragment : Fragment() {
-    private var _binding: FragmentListaDeNotasBinding? = null
-    private val binding get() = _binding!!
+class ListaDeNotasAdapter :
+    ListAdapter<Nota, ListaDeNotasAdapter.ItemNotaViewHolder>(NotaDiffUtil) {
+    private var accionAlHacerClic: ((String) -> Unit)? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentListaDeNotasBinding.inflate(inflater, container, false)
-        return binding.root
+    fun setAccionAlHacerClic(accion: (String) -> Unit) {
+        accionAlHacerClic = accion
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemNotaViewHolder {
+        return ItemNotaViewHolder(
+            ItemNotaBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onBindViewHolder(holder: ItemNotaViewHolder, i: Int) {
+        val nota = getItem(i)
+        holder.enlazar(nota)
+    }
+
+    inner class ItemNotaViewHolder(binding: ItemNotaBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        private val contenedor = binding.contenedorItemNota
+        private val titulo = binding.tituloNota
+        private val contenido = binding.contenidoNota
+        private val fondoNotas = contenedor.background as GradientDrawable
+        private var notaActual: Nota? = null
+
+        init {
+            itemView.setOnClickListener {
+                notaActual?.let { notaActual ->
+                    accionAlHacerClic?.let { it(notaActual.id) }
+                }
+            }
+        }
+
+        fun enlazar(nota: Nota) {
+            fondoNotas.color = AppCompatResources.getColorStateList(itemView.context, nota.color)
+            notaActual = nota
+
+            if (nota.color == android.R.color.background_light)
+                fondoNotas.setStroke(
+                    2,
+                    ContextCompat.getColor(itemView.context, android.R.color.background_dark)
+                )
+            else
+                fondoNotas.setStroke(2, ContextCompat.getColor(itemView.context, nota.color))
+
+            if (nota.titulo.isNotEmpty()) {
+                titulo.isVisible = true
+                titulo.text = nota.titulo
+            }
+            if (nota.contenido.isNotEmpty()) {
+                contenido.isVisible = true
+                contenido.text = nota.contenido
+            }
+        }
+    }
+
+    object NotaDiffUtil : DiffUtil.ItemCallback<Nota>() {
+        override fun areItemsTheSame(oldItem: Nota, newItem: Nota): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Nota, newItem: Nota): Boolean {
+            return oldItem.id == newItem.id
+        }
+
     }
 }
