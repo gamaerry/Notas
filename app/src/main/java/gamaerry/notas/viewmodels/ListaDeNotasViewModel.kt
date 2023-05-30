914414ef1fa6f11d4aa54bf63bd3e771ff3abfd3
gamaerry <gamaerry@gmail.com>
2023-05-30 10:51:00 -0600

    obtencion de lista de notas estaticas simplificada

--- app/src/main/java/gamaerry/notas/viewmodels/ListaDeNotasViewModel.kt
+++ app/src/main/java/gamaerry/notas/viewmodels/ListaDeNotasViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import gamaerry.notas.datos.Nota
 import gamaerry.notas.datos.RepositorioDeNotas
+import gamaerry.notas.datos.getListaDeNotas
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.launchIn
@@ -17,12 +18,13 @@ class ListaDeNotasViewModel
 constructor(
     private val repositorio: RepositorioDeNotas
 ) : ViewModel() {
-    private var _listaDeNotas = MutableStateFlow<List<Nota>>(emptyList())
+//    private var _listaDeNotas = MutableStateFlow<List<Nota>>(emptyList())
+    private var _listaDeNotas = MutableStateFlow(getListaDeNotas()) //es posible pasar directamente la lista de notas
     val listaDeNotas: StateFlow<List<Nota>> get() = _listaDeNotas
 
-    init {
-        getNotas()
-    }
+//    init {
+//        getNotas()
+//    }
 
     private fun getNotas() {
         repositorio.getFlujoDeListaDeNotas().onEach {
