975dfcc740d07a2d8bc2dabb598cc3f455267dbb
gamaerry <gamaerry@gmail.com>
2023-05-30 22:45:32 -0600

    enfoque establecido al contenido de la nota

--- app/src/main/java/gamaerry/notas/fragmentos/DetalleDeNotaFragment.kt
+++ app/src/main/java/gamaerry/notas/fragmentos/DetalleDeNotaFragment.kt
@@ -7,11 +7,17 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 import gamaerry.notas.databinding.FragmentDetalleDeNotaBinding
+import gamaerry.notas.utilidades.mostrarTeclado
 
 @AndroidEntryPoint
 class DetalleDeNotaFragment : Fragment() {
     private var _binding: FragmentDetalleDeNotaBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.contenidoNota.mostrarTeclado()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,11 +26,6 @@ class DetalleDeNotaFragment : Fragment() {
     ): View {
         _binding = FragmentDetalleDeNotaBinding.inflate(inflater, container, false)
         return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
     }
 
     override fun onDestroyView() {
