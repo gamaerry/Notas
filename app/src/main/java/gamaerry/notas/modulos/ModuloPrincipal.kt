371ceed34e4799631e33ca9511c88f4396f10ebf
gamaerry <gamaerry@gmail.com>
2023-06-01 23:18:27 -0600

    creados los colores para el SelectorDeColorAdapter

--- app/src/main/java/gamaerry/notas/modulos/ModuloPrincipal.kt
+++ app/src/main/java/gamaerry/notas/modulos/ModuloPrincipal.kt
@@ -4,11 +4,30 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import gamaerry.notas.R
 import gamaerry.notas.adaptadores.ListaDeNotasAdapter
+import gamaerry.notas.adaptadores.SelectorDeColorAdapter
 import gamaerry.notas.datos.RepositorioDeNotas
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ModuloPrincipal {
+    @Provides
+    @Singleton
+    fun proveerSelectorDeColorAdapter() = SelectorDeColorAdapter(
+        listOf(
+            R.color.rojo,
+            R.color.rosa,
+            R.color.naranja1,
+            R.color.naranja2,
+            R.color.amarillo,
+            R.color.verde1,
+            R.color.verde2,
+            R.color.verde3,
+            R.color.azul1,
+            R.color.azul2,
+            R.color.morado,
+        )
+    )
 }
