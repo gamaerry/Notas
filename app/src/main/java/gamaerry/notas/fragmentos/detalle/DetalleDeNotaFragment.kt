2457c983bac2c5b2f32c399b0a06d94676fc811f
gamaerry <gamaerry@gmail.com>
2023-06-03 12:58:47 -0600

    cambiado de color del background exitoso

--- app/src/main/java/gamaerry/notas/fragmentos/detalle/DetalleDeNotaFragment.kt
+++ app/src/main/java/gamaerry/notas/fragmentos/detalle/DetalleDeNotaFragment.kt
@@ -4,16 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import gamaerry.notas.R
 import gamaerry.notas.databinding.FragmentDetalleDeNotaBinding
 import gamaerry.notas.utilidades.mostrarTeclado
+import gamaerry.notas.viewmodels.DetalleDeNotaViewModel
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class DetalleDeNotaFragment : Fragment() {
     private var _binding: FragmentDetalleDeNotaBinding? = null
     private val binding get() = _binding!!
+    private val detalleDeNotaViewModel: DetalleDeNotaViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -21,6 +29,16 @@ class DetalleDeNotaFragment : Fragment() {
         binding.colorDeNota.setOnClickListener {
             SelectorDeColorFragment().show(requireActivity().supportFragmentManager, "selector")
         }
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.CREATED) {
+                detalleDeNotaViewModel.colorSeleccionado.collect {
+                    binding.parentDetalleDeNota.setBackgroundColor(
+                        ContextCompat.getColor(requireContext(), it)
+                    )
+                }
+            }
+        }
+
     }
 
     override fun onCreateView(
