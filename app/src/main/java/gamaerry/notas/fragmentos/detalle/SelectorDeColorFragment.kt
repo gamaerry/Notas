9c239e82462d76334d303a4d719f28902a56dce9
gamaerry <gamaerry@gmail.com>
2023-06-03 12:46:03 -0600

    implementado el viewModel en el fragment selector

--- app/src/main/java/gamaerry/notas/fragmentos/detalle/SelectorDeColorFragment.kt
+++ app/src/main/java/gamaerry/notas/fragmentos/detalle/SelectorDeColorFragment.kt
@@ -3,11 +3,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 import gamaerry.notas.adaptadores.SelectorDeColorAdapter
 import gamaerry.notas.databinding.FragmentSelectorDeColorBinding
+import gamaerry.notas.viewmodels.DetalleDeNotaViewModel
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -15,9 +17,14 @@ class SelectorDeColorFragment : BottomSheetDialogFragment() {
     private var _binding: FragmentSelectorDeColorBinding? = null
     private val binding get() = _binding!!
     @Inject lateinit var selectorDeColorAdapter: SelectorDeColorAdapter
+    private val detalleDeNotaViewModel: DetalleDeNotaViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        selectorDeColorAdapter.setAccionAlHacerClic {
+            detalleDeNotaViewModel.actualizarColor(it)
+            this.dismiss()
+        }
         binding.listaDeColores.apply {
             layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
             adapter = selectorDeColorAdapter
