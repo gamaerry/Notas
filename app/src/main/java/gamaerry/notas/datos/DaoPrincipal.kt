f860f3f14725a63241f01a0e57e1f226bd8cbce0
gamaerry <gamaerry@gmail.com>
2023-06-03 19:43:44 -0600

    operacion Insertar agregada al Dao

--- app/src/main/java/gamaerry/notas/datos/DaoPrincipal.kt
+++ app/src/main/java/gamaerry/notas/datos/DaoPrincipal.kt
@@ -1,7 +1,11 @@
 package gamaerry.notas.datos
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 
 @Dao
 interface DaoPrincipal {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun operacionInsertarNota(nota: Nota): Long
 }
