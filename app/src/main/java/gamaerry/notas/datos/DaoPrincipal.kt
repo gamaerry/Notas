dfd7da4b50745fe8356028a8d2fef3a784119c64
gamaerry <gamaerry@gmail.com>
2023-06-04 12:40:05 -0600

    operacionGetListaDeNotas agregada al Dao

--- app/src/main/java/gamaerry/notas/datos/DaoPrincipal.kt
+++ app/src/main/java/gamaerry/notas/datos/DaoPrincipal.kt
@@ -3,10 +3,14 @@ package gamaerry.notas.datos
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import gamaerry.notas.modelo.Nota
 
 @Dao
 interface DaoPrincipal {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun operacionInsertarNota(nota: Nota): Long
+
+    @Query("SELECT * FROM Nota WHERE titulo LIKE '%' || :query || '%' OR contenido LIKE '%' || :query || '%'")
+    suspend fun operacionGetListaDeNotas(query: String): List<Nota>
 }
