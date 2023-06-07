acda9afaed310675ea0b6d5f9e45f0e2082cb8f1
gamaerry <gamaerry@gmail.com>
2023-06-07 14:51:20 -0600

    implementada la accionAlHacerClic del ListaDeNotasFragment

--- app/src/main/java/gamaerry/notas/fragmentos/ListaDeNotasFragment.kt
+++ app/src/main/java/gamaerry/notas/fragmentos/ListaDeNotasFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -15,6 +17,7 @@ import gamaerry.notas.R
 import gamaerry.notas.adaptadores.ListaDeNotasAdapter
 import gamaerry.notas.cambiarColorDelStatusBar
 import gamaerry.notas.databinding.FragmentListaDeNotasBinding
+import gamaerry.notas.viewmodels.DetalleDeNotaViewModel
 import gamaerry.notas.viewmodels.ListaDeNotasViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -28,6 +31,7 @@ class ListaDeNotasFragment : Fragment() {
     // con by viewModels() uso la implementacion que me permite usar
     // este objeto exclusivamente dentro del alcance de este fragment
     private val listaDeNotasViewModel: ListaDeNotasViewModel by viewModels()
+    private val detalleDeNotaViewModel: DetalleDeNotaViewModel by activityViewModels()
 
     // gracias a la inyeccion de dependencias no tengo que usar el constructor
     @Inject
@@ -38,32 +42,40 @@ class ListaDeNotasFragment : Fragment() {
         // actualizar color y notas (obtenidas estas por el viewModel)
         requireActivity().window.cambiarColorDelStatusBar(R.color.principal)
         listaDeNotasViewModel.getNotas()
-        // establecer accion para el boton de nueva nota
-        binding.nuevaNota.setOnClickListener {
-            requireActivity().supportFragmentManager.beginTransaction().apply {
-                // establece las animaciones tanto para el
-                // fragmento que va saliendo de escena,
-                // como para el que va entrando
-                setCustomAnimations(
-                    R.anim.entrar_desde_derecha,
-                    R.anim.salir_hacia_izquierda,
-                    R.anim.entrar_desde_izquierda,
-                    R.anim.salir_hacia_derecha
-                )
-                // haciendo uso de replace() hago que el fragment que va saliendo
-                // se destruya completamente, eliminando por completo la
-                // responsabilidad del gestor de recursos de android de
-                // mantener vivo al fragmento (como sucede con add())
-                replace(R.id.contenedorPrincipal, DetalleDeNotaFragment())
-                // no obstante la sentencia anterior, con addToBackStack()
-                // me aseguro que a la hora de realizar el gesto de back
-                // en el dispositivo, me regrese (cree de nuevo) a mi
-                // fragmento anterior (ListaDeNotasFragment en este caso)
-                addToBackStack("detalleNuevaNota")
-                // inicio la transición
-                commit()
-            } // TODO: probablemente sería mas adecuado si creo el objeto FragmentTransaction y como ejecuto commit() a la hora de pulsar nuevaNota
+
+        // esta transicion se hace independientemente si
+        // se trata de una nueva nota o una ya existente
+        val transicion = requireActivity().supportFragmentManager.beginTransaction().apply {
+            // establece las animaciones tanto para el
+            // fragmento que va saliendo de escena,
+            // como para el que va entrando
+            setCustomAnimations(
+                R.anim.entrar_desde_derecha,
+                R.anim.salir_hacia_izquierda,
+                R.anim.entrar_desde_izquierda,
+                R.anim.salir_hacia_derecha
+            )
+            // haciendo uso de replace() hago que el fragment que va saliendo
+            // se destruya completamente, eliminando por completo la
+            // responsabilidad del gestor de recursos de android de
+            // mantener vivo al fragmento (como sucede con add())
+            replace(R.id.contenedorPrincipal, DetalleDeNotaFragment())
+            // no obstante la sentencia anterior, con addToBackStack()
+            // me aseguro que a la hora de realizar el gesto de back
+            // en el dispositivo, me regrese (cree de nuevo) a mi
+            // fragmento anterior (ListaDeNotasFragment en este caso)
+            addToBackStack("detalleNota")
         }
+
+        // establecer accion para el boton de nueva nota
+        binding.nuevaNota.setOnClickListener { transicion.commit() }
+
+        // hace la misma transicion pero con la nota seleccionada
+        listaDeNotasAdapter.accionAlHacerClic = {
+            detalleDeNotaViewModel.setNotaPorId(it)
+            transicion.commit()
+        }
+
         // pasar las notas del viewModel al listAdapter
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.CREATED) {
